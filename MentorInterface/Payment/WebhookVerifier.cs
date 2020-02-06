cad357b4553bc763f4c3b8a5a573e65db709919e
Arran <code@arran.nz>
2020-02-06 19:19:03 +0100

    Add framework to support webhook alerts

---
+++ MentorInterface/Payment/WebhookVerifier.cs
@@ -0,0 +1,40 @@
+﻿using MentorInterface.Payment.IncomingModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorInterface.Payment
+{
+    /// <summary>
+    /// Verify Paddle Webhooks
+    /// https://developer.paddle.com/webhook-reference/verifying-webhooks
+    /// </summary>
+    public class WebhookVerifier
+    {
+
+        private string publicKey { get; }
+
+        /// <summary>
+        /// Initialize with a public key provided by Paddle.
+        /// </summary>
+        /// <param name="publicKey"></param>
+        public WebhookVerifier(string publicKey)
+        {
+            this.publicKey = publicKey;
+        }
+
+
+        /// <summary>
+        /// Verify a message from Paddle
+        /// </summary>
+        public bool IsAlertValid(IPaddleAlert alert)
+        {
+            string json = JsonConvert.SerializeObject(alert);
+            return false;
+        }
+
+
+    }
+}
