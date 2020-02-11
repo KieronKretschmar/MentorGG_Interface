da7f18c28263b333594db6b4299a8fc8089dd698
Arran <code@arran.nz>
2020-02-11 13:27:41 +0100

    Implement PaddleAlert handling

--- MentorInterface/Controllers/Webhooks.cs
+++ MentorInterface/Controllers/Webhooks.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Database;
+using Entities.Models.Paddle;
+using Entities.Models.Paddle.Alerts;
+using MentorInterface.Helpers.ModelFactories;
+using MentorInterface.Helpers.ModelFactories.Paddle;
 using MentorInterface.Payment;
-using MentorInterface.Payment.IncomingModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace MentorInterface.Controllers
@@ -17,78 +22,146 @@ namespace MentorInterface.Controllers
     public class Webhooks : ControllerBase
     {
 
-        WebhookVerifier _webhookVerifier;
+        readonly WebhookVerifier _webhookVerifier;
+        private readonly ILogger<Webhooks> _logger;
+        private readonly ApplicationContext _applicationContext;
+
         /// <summary>
         ///
         /// </summary>
-        public Webhooks()
+        public Webhooks(ILogger<Webhooks> logger,
+            ApplicationContext applicationContext)
         {
+
             _webhookVerifier = new WebhookVerifier("x");
+            _logger = logger;
+            _applicationContext = applicationContext;
         }
 
         /// <summary>
         /// Paddle Webhook Receiver.
         /// </summary>
-        /// <param name="formContent">Form Content</param>
+        /// <param name="rawAlert">Form Content</param>
         /// <returns></returns>
         [HttpPost("paddle")]
         [Consumes("application/x-www-form-urlencoded")]
-        public IActionResult HandleAlert([FromForm]Dictionary<string, string> formContent)
+        public IActionResult HandleAlert([FromForm]Dictionary<string, string> rawAlert)
         {
             string alertName;
             try
             {
-                alertName = formContent["alert_name"];
+                alertName = rawAlert["alert_name"];
             }
             catch (KeyNotFoundException)
             {
                 return StatusCode(400);
             }
 
-            switch (alertName)
+            // Confirm the alert is valid
+            if (!_webhookVerifier.IsAlertValid(rawAlert))
             {
-                case AlertType.SubscriptionCreated:
-
-                    SubscriptionCreated alert;
-                    try
-                    {
-                        alert = (SubscriptionCreated)formContent;
-                    }
-                    catch (KeyNotFoundException)
-                    {
-                        return StatusCode(400);
-                    }
-
-                    if (_webhookVerifier.IsAlertValid(alert))
-                    {
-                        // Upgrade user to prem etc. etc.
-                    }
-                    else
-                    {
-                        // Signature mismatch
-                        return StatusCode(401);
-                    }
-
-                    return StatusCode(200);
-
-                case AlertType.SubscriptionUpdated:
-                    return StatusCode(200);
-
-                case AlertType.SubscriptionCancelled:
-                    return StatusCode(200);
-
-                case AlertType.SubscriptionPaymentFailed:
-                    return StatusCode(200);
-
-                case AlertType.SubscriptionPaymentSucceded:
-                    return StatusCode(200);
-
-                case AlertType.SubscriptionPaymentRefunded:
-                    return StatusCode(200);
-
-                default:
-                    return StatusCode(501);
+                // Signature mismatch
+                return StatusCode(401);
             }
+
+            try
+            {
+                switch (alertName)
+                {
+                    case AlertType.SubscriptionCreated:
+                        var createdAlert = SubscriptionCreatedFactory.FromAlert(rawAlert);
+                        return CreateSubscription(createdAlert);
+
+                    case AlertType.SubscriptionUpdated:
+                        var updatedAlert = SubscriptionUpdatedFactory.FromAlert(rawAlert);
+                        return UpdateSubscription(updatedAlert);
+
+                    case AlertType.SubscriptionCancelled:
+                        var cancelledAlert = SubscriptionCancelledFactory.FromAlert(rawAlert);
+                        return CancelSubscription(cancelledAlert);
+
+                    case AlertType.SubscriptionPaymentSucceded:
+                        var paymentSucceededAlert = SubscriptionPaymentSucceededFactory.FromAlert(rawAlert);
+                        _applicationContext.SubscriptionPaymentSucceeded.Add(paymentSucceededAlert);
+                        _applicationContext.SaveChanges();
+                        return StatusCode(200);
+
+                    case AlertType.SubscriptionPaymentFailed:
+                        var paymentFailedAlert = SubscriptionPaymentFailedFactory.FromAlert(rawAlert);
+                        _applicationContext.SubscriptionPaymentFailed.Add(paymentFailedAlert);
+                        _applicationContext.SaveChanges();
+                        return StatusCode(200);
+
+                    case AlertType.SubscriptionPaymentRefunded:
+                        var paymentRefundedAlert = SubscriptionPaymentRefundedFactory.FromAlert(rawAlert);
+                        _applicationContext.SubscriptionPaymentRefunded.Add(paymentRefundedAlert);
+                        _applicationContext.SaveChanges();
+                        return StatusCode(200);
+
+                    default:
+                        return StatusCode(501);
+                }
+            }
+            catch (AlertParseException ex)
+            {
+                _logger.LogError(ex, "Failed to parse alert from Paddle");
+                return StatusCode(400);
+            }
+
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="alert"></param>
+        /// <returns></returns>
+        private IActionResult CreateSubscription(SubscriptionCreated alert)
+        {
+            _applicationContext.SubscriptionCreated.Add(alert);
+
+            PaddleUser existingUser = _applicationContext.PaddleUser.SingleOrDefault(
+                x => x.UserId == alert.UserId);
+
+            if (existingUser != null)
+            {
+                _logger.LogError(
+                    $"Tried to create subscription for pre-existing user: [ {existingUser.UserId} ]");
+                return StatusCode(400);
+            }
+
+            var newUser = PaddleUserFactory.FromAlert(alert);
+            _applicationContext.PaddleUser.Add(newUser);
+
+            _applicationContext.SaveChanges();
+            return StatusCode(200);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="alert"></param>
+        /// <returns></returns>
+        private IActionResult UpdateSubscription(SubscriptionUpdated alert)
+        {
+            _applicationContext.SubscriptionUpdated.Add(alert);
+
+            PaddleUser userInfo = PaddleUserFactory.FromAlert(alert);
+            _applicationContext.UpdatePaddleUser(userInfo);
+            return StatusCode(200);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="alert"></param>
+        /// <returns></returns>
+        private IActionResult CancelSubscription(SubscriptionCancelled alert)
+        {
+            _applicationContext.SubscriptionCancelled.Add(alert);
+
+            PaddleUser userInfo = PaddleUserFactory.FromCancelledAlert(alert);
+            _applicationContext.UpdatePaddleUser(userInfo);
+            return StatusCode(200);
         }
     }
 }
