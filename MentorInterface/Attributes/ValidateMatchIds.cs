ea38bb17dc0a1fac4067c93dd88e32d345dd1352
Kieron <kieron@mentor.gg>
2020-02-13 16:09:47 +0100

    Add ValidateMatchIds attribute

---
+++ MentorInterface/Attributes/ValidateMatchIds.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorInterface.Attributes
+{
+    /// <summary>
+    /// An ActionFilter that validates the string parameter matchIds to be made up of numbers seperated by the defined Seperator.
+    /// </summary>
+    public class ValidateMatchIds : ActionFilterAttribute
+    {
+        /// <summary>
+        /// The seperator used for dividing different matchids.
+        /// </summary>
+        private const char Seperator = ',';
+
+        /// <summary>
+        /// The name of the matchIds argument.
+        /// </summary>
+        public static readonly string MatchIdsArgument = "matchIds";
+
+        /// <summary>
+        /// OnActionExecuting override.
+        /// </summary>
+        /// <param name="context"></param>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var matchIds = context.ActionArguments[MatchIdsArgument].ToString();
+            var isValid = matchIds.Split(Seperator).All(x => x.All(c => Char.IsDigit(c)));
+
+            if (!isValid)
+            {
+                context.Result = new BadRequestResult();
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
