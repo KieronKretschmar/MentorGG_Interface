7ece6cdf3f996a14007ca23db29458eece2fd8d7
Kieron <kieron@mentor.gg>
2020-02-13 18:52:35 +0100

    Add MatchRetriever endpoints

---
+++ MentorInterface/Controllers/MatchData/SmokesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Entities.Models;
+using MentorInterface.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MentorInterface.Controllers.MatchData
+{
+    /// <summary>
+    /// Smokes controller.
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/")]
+    public class SmokesController : ForwardController
+    {
+        /// <summary>
+        /// Http Client Factory
+        /// </summary>
+        private readonly IHttpClientFactory _clientFactory;
+
+        /// <summary>
+        /// User Manager
+        /// </summary>
+        private readonly UserManager<ApplicationUser> _userMananger;
+
+        /// <summary>
+        /// Create the controller and inject the HTTPClient factory.
+        /// </summary>
+        public SmokesController(
+            IHttpClientFactory clientFactory,
+            UserManager<ApplicationUser> userManager)
+        {
+            _clientFactory = clientFactory;
+            _userMananger = userManager;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet("single/{steamId}/smokes")]
+        public async Task<IActionResult> SmokesAsync(long steamId, string matchIds, string map)
+        {
+            var client = _clientFactory.CreateClient(ConnectedServices.MatchRetriever);
+
+            HttpRequestMessage message = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"v1/public/single/{steamId}/smokes?matchIds={matchIds}&map={map}");
+
+            return await ForwardHttpRequest(client, message);
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet("single/{steamId}/smokesoverview")]
+        public async Task<IActionResult> SmokesOverviewAsync(long steamId, string matchIds)
+        {
+            var client = _clientFactory.CreateClient(ConnectedServices.MatchRetriever);
+
+            HttpRequestMessage message = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"v1/public/single/{steamId}/smokesoverview?matchIds={matchIds}");
+
+            return await ForwardHttpRequest(client, message);
+        }
+    }
+}
