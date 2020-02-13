9ea61a852259994b5ed6b6d3ff07a34a8b639bcd
Kieron <kieron@mentor.gg>
2020-02-13 16:10:13 +0100

    Add Test project with test for ValidateMatchIds attribute

---
+++ MentorInterfaceTestProject/TestHelper.cs
@@ -0,0 +1,40 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MentorInterfaceTestProject
+{
+    static class TestHelper
+    {
+        /// <summary>
+        /// Returns (unconfigured) Mock of UserManager.
+        /// </summary>
+        /// <returns></returns>
+        public static Mock<UserManager<ApplicationUser>> GetUserManagerMock()
+        {
+            var mockUserManager = new Mock<UserManager<ApplicationUser>>(
+                    new Mock<IUserStore<ApplicationUser>>().Object,
+                    new Mock<IOptions<IdentityOptions>>().Object,
+                    new Mock<IPasswordHasher<ApplicationUser>>().Object,
+                    new IUserValidator<ApplicationUser>[0],
+                    new IPasswordValidator<ApplicationUser>[0],
+                    new Mock<ILookupNormalizer>().Object,
+                    new Mock<IdentityErrorDescriber>().Object,
+                    new Mock<IServiceProvider>().Object,
+                    new Mock<ILogger<UserManager<ApplicationUser>>>().Object);
+
+            return mockUserManager;
+
+
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            var userManagerMock = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object);
+            userManagerMock.Setup(x => x.FindByIdAsync(It.IsAny<string>())).Returns(() => null);
+            return userManagerMock;
+        }
+    }
+}
