8614aac30ec0eaddf70f2a9c7b51423ec2a305b2
Kieron <kieron@mentor.gg>
2020-02-14 17:43:06 +0100

    Add PlayerInfoController

---
+++ MentorInterface/Controllers/MatchData/PlayerInfoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Entities.Models;
+using MentorInterface.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MentorInterface.Controllers.MatchData
+{
+    /// <summary>
+    /// Smokes controller.
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/")]
+    public class PlayerInfoController : ForwardController
+    {
+        /// <summary>
+        /// Http Client Factory
+        /// </summary>
+        private readonly IHttpClientFactory _clientFactory;
+
+        /// <summary>
+        /// User Manager
+        /// </summary>
+        private readonly UserManager<ApplicationUser> _userMananger;
+
+        /// <summary>
+        /// Create the controller and inject the HTTPClient factory.
+        /// </summary>
+        public PlayerInfoController(
+            IHttpClientFactory clientFactory,
+            UserManager<ApplicationUser> userManager)
+        {
+            _clientFactory = clientFactory;
+            _userMananger = userManager;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet("single/{steamId}/playerinfo")]
+        public async Task<IActionResult> PlayerInfoAsync(long steamId)
+        {
+            var client = _clientFactory.CreateClient(ConnectedServices.MatchRetriever);
+
+            HttpRequestMessage message = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"v1/public/single/{steamId}/playerinfo");
+
+            return await ForwardHttpRequest(client, message);
+        }
+    }
+}
