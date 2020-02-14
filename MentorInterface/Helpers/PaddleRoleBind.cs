17a0a2aecbae74d2abe8fcb99a516613825fbec0
Arran <code@arran.nz>
2020-02-14 18:23:06 +0100

    Add Role Helpers

---
+++ MentorInterface/Helpers/PaddleRoleBind.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorInterface.Helpers
+{
+    /// <summary>
+    /// Paddle Role Bind
+    /// Bind a PaddlePlanId to a Application Role
+    /// </summary>
+    public struct PaddleRoleBind
+    {
+        public readonly int PlanId;
+
+        public readonly string RoleName;
+
+        /// <summary>
+        /// Create a RoleBind
+        /// </summary>
+        public PaddleRoleBind(int planId, string roleName)
+        {
+            PlanId = planId;
+            RoleName = roleName;
+        }
+    }
+}
