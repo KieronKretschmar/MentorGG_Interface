b7793f7567c2c9d1b5cef26d8a96c34a90788dff
Arran <code@arran.nz>
2020-02-17 17:27:45 +0100

    Get by PaddleUserId in PaddleUserMananger

--- MentorInterface/Paddle/PaddleUserMananger.cs
+++ MentorInterface/Paddle/PaddleUserMananger.cs
@@ -8,33 +8,55 @@ using System.Threading.Tasks;
 
 namespace MentorInterface.Paddle
 {
+    /// <summary>
+    /// Manages PaddleUsers in the DatabaseContext
+    /// </summary>
     public class PaddleUserMananger
     {
         private readonly ApplicationContext _applicationContext;
 
         private DbSet<PaddleUser> Context => _applicationContext.PaddleUser;
 
+        /// <summary>
+        /// Stores a reference to the DatabaseContext
+        /// </summary>
+        /// <param name="applicationContext"></param>
         public PaddleUserMananger(ApplicationContext applicationContext)
         {
             this._applicationContext = applicationContext;
         }
 
+        /// <summary>
+        /// Return a PaddleUser
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<PaddleUser> GetUserAsync(int userId)
         {
-            throw new NotImplementedException();
+            return await Context.SingleAsync<PaddleUser>(x => x.UserId == userId);
         }
 
+        /// <summary>
+        /// Add a PaddleUser
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
         public async Task<bool> AddUserAsync(PaddleUser userInfo)
         {
             await Context.AddAsync(userInfo);
             return (await _applicationContext.SaveChangesAsync()) > 0;
         }
 
+        /// <summary>
+        /// Retreive an existing PaddleUser and update.
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
         public async Task<bool> UpdateUserAsync(PaddleUser userInfo)
         {
             // Get the existing user by the UserId
             PaddleUser existingUser = await Context.SingleAsync(
-                x => x.ApplicationUserId == userInfo.ApplicationUserId);
+                x => x.UserId == userInfo.UserId);
 
             // Iterate over each property and assign each field in the
             // existing user -  Except the PK (Id)
@@ -50,9 +72,14 @@ namespace MentorInterface.Paddle
             return (await _applicationContext.SaveChangesAsync()) > 0;
         }
 
-        public async Task<bool> RemoveUserAsync(PaddleUser userInfo)
+        /// <summary>
+        /// Remove a PaddleUser.
+        /// </summary>
+        /// <param name="userInfo"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveUserAsync(int userId)
         {
-            Context.Remove(userInfo);
+            Context.Remove(await GetUserAsync(userId));
             return (await _applicationContext.SaveChangesAsync()) > 0;
         }
     }
