21cb881afbbb45d06eb1fd9439dd871e4599a789
Kieron <kieron@mentor.gg>
2020-02-28 18:01:56 +0100

    Add ImportantPositionsController

---
+++ MentorInterface/Controllers/MatchData/ImportantPositionsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Entities.Models;
+using MentorInterface.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MentorInterface.Controllers.MatchData
+{
+    /// <summary>
+    /// FriendComparison controller.
+    /// </summary>
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/")]
+    public class ImportantPositionsController : ForwardController
+    {
+        /// <summary>
+        /// Http Client Factory
+        /// </summary>
+        private readonly IHttpClientFactory _clientFactory;
+
+        /// <summary>
+        /// User Manager
+        /// </summary>
+        private readonly UserManager<ApplicationUser> _userMananger;
+
+        /// <summary>
+        /// Create the controller and inject the HTTPClient factory.
+        /// </summary>
+        public ImportantPositionsController(
+            IHttpClientFactory clientFactory,
+            UserManager<ApplicationUser> userManager)
+        {
+            _clientFactory = clientFactory;
+            _userMananger = userManager;
+        }
+
+        /// <summary>
+        /// Get FriendsComparison
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet("single/{steamId}/importantpositions")]
+        public async Task<IActionResult> ImportantPositionsAsync(long steamId, string matchIds, bool showBest, int count = 3)
+        {
+            var client = _clientFactory.CreateClient(ConnectedServices.MatchRetriever);
+
+            HttpRequestMessage message = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"v1/public/single/{steamId}/importantpositions?matchIds={matchIds}&showbest={showBest}&count={count}");
+
+            return await ForwardHttpRequest(client, message);
+        }
+    }
+}
