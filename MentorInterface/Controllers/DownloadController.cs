99ba6da1f9b7630cfd9300907edfa6c887dd9704
Kieron <kieron@mentor.gg>
2020-05-06 14:51:53 +0200

    Add DownloadController with endpoint for providing  blobUrls

---
+++ MentorInterface/Controllers/DownloadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MentorInterface.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentorInterface.Controllers
+{
+    [Route("v{version:apiVersion}/")]
+    [ApiController]
+    public class DownloadController : ForwardController
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public DownloadController(
+            IHttpClientFactory clientFactory
+            )
+        {
+            _clientFactory = clientFactory;
+        }
+
+
+        /// <summary>
+        /// Get blobUrl of demo file.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("match/{matchId}/download-url")]
+        public async Task<IActionResult> DownloadUrlAsync(long matchId)
+        {
+            var client = _clientFactory.CreateClient(ConnectedServices.DemoCentral);
+
+            HttpRequestMessage message = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"/v1/public/match/{matchId}/download-url");
+
+            return await ForwardHttpRequest(client, message);
+        }
+    }
+}
