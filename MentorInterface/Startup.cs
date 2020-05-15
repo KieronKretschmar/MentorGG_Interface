779753ae4fd25dd9348a05917058a6f7a93b171d
Arran <code@arran.nz>
2020-05-15 15:21:31 +0200

    Remove debug console doubleup

--- MentorInterface/Startup.cs
+++ MentorInterface/Startup.cs
@@ -141,12 +141,6 @@ namespace MentorInterface
                     o.TimestampFormat = "[yyyy-MM-dd HH:mm:ss zzz] ";
                 });
 
-                o.AddConsole(options =>
-                {
-                    options.TimestampFormat = "[yyyy-MM-dd HH:mm:ss zzz] ";
-                });
-                o.AddDebug();
-
                 // Filter out ASP.Net and EFCore logs of LogLevel lower than LogLevel.Warning
                 o.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning);
                 o.AddFilter("Microsoft.EntityFrameworkCore.Infrastructure", LogLevel.Warning);
