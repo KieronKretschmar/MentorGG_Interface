60790052fdb36cfb2c5c9acee1756e6104a5252c
Arran <code@arran.nz>
2020-07-02 14:17:27 +0200

    Add Bulk endpoint to gather UserIdentities

--- MentorInterface/Helpers/RoleHelper.cs
+++ MentorInterface/Helpers/RoleHelper.cs
@@ -23,6 +23,13 @@ namespace MentorInterface.Helpers
         Task<int> GetDailyMatchesLimitAsync(ApplicationUser user);
 
         /// <summary>
+        /// Gets the number of matches a list of users may analyze daily.
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        Task<List<Tuple<ApplicationUser, int>>> GetDailyMatchesLimitAsync(List<ApplicationUser> users);
+
+        /// <summary>
         /// Gets the user's subscription. Returns the highest one if multiple are available.
         /// </summary>
         /// <param name="user"></param>
@@ -38,6 +45,13 @@ namespace MentorInterface.Helpers
         /// <param name="requestedSteamId"></param>
         /// <returns></returns>
         Task<SubscriptionType> GetSubscriptionTypeAsync(ApplicationUser user, long requestedSteamId);
+
+        /// <summary>
+        /// Return a SubscriptionType for a list of ApplicationUsers
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        Task<List<Tuple<ApplicationUser, SubscriptionType>>> GetSubscriptionTypesAsync(List<ApplicationUser> users);
     }
 
     public class RoleHelper : IRoleHelper
@@ -79,11 +93,22 @@ namespace MentorInterface.Helpers
                 .Max(x => (int?) x.DailyMatchesLimit) ?? DAILY_LIMIT_DEFAULT;
         }
 
+        public async Task<List<Tuple<ApplicationUser, int>>> GetDailyMatchesLimitAsync(List<ApplicationUser> users)
+        {
+            var limits = new List<Tuple<ApplicationUser, int>>(users.Count);
+            foreach(var user in users)
+            {
+                var limit = await GetDailyMatchesLimitAsync(user);
+                limits.Add(new Tuple<ApplicationUser, int>(user, limit));
+            }
+            return limits;
+        }
+
         public async Task<SubscriptionType> GetSubscriptionTypeAsync(ApplicationUser user)
         {
             var roles = await _userManager.GetRolesAsync(user);
 
-            if(roles.Contains(RoleCreator.Ultimate.Name))
+            if( roles.Contains(RoleCreator.Ultimate.Name))
             {
                 return SubscriptionType.Ultimate;
             }
@@ -92,11 +117,10 @@ namespace MentorInterface.Helpers
                 return SubscriptionType.Premium;
             }
 
-            // Check whether user is Influencer by having MENTOR.GG in their name as we currently do not store it as a role in database
             var steamUserData = await _steamUserOperator.GetUser(user.SteamId);
-            if (steamUserData.SteamName.ToLowerInvariant().Contains("mentor.gg"))
+            if (IsInfluencer(steamUserData.SteamName))
             {
-                _logger.LogInformation($"User [ {steamUserData.SteamId} ] has mentor.gg in his name.");
+                _logger.LogInformation($"User [ {user.SteamId} ] has mentor.gg in his name - Returning SubscriptioType.Influencer");
                 return SubscriptionType.Influencer;
             }
 
@@ -111,6 +135,49 @@ namespace MentorInterface.Helpers
             }
 
             return await GetSubscriptionTypeAsync(user);
+        }
+
+        public async Task<List<Tuple<ApplicationUser, SubscriptionType>>> GetSubscriptionTypesAsync(List<ApplicationUser> users)
+        {
+            var subscriptionTypes = new List<Tuple<ApplicationUser, SubscriptionType>>(users.Count);
+            
+            // Determine of which users are considered Influencers
+            var steamUsers = await _steamUserOperator.GetUsers(users.Select(x=>x.SteamId).ToList());
+            var influencers = steamUsers.Where(x => IsInfluencer(x.SteamName)).ToList();
+
+            //Iterate over each Application user and assign a SubscriptionType
+            //Based on Role information and Influencer info
+            foreach(var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (roles.Contains(RoleCreator.Ultimate.Name))
+                {
+                    subscriptionTypes.Add(new Tuple<ApplicationUser, SubscriptionType>(user, SubscriptionType.Ultimate));
+                    continue;
+                }
+                if (roles.Contains(RoleCreator.Premium.Name))
+                {
+                    subscriptionTypes.Add(new Tuple<ApplicationUser, SubscriptionType>(user, SubscriptionType.Premium));
+                    continue;
+                }
+
+                if (influencers.Select(x => x.SteamId).Contains(user.SteamId))
+                {
+                    subscriptionTypes.Add(new Tuple<ApplicationUser, SubscriptionType>(user, SubscriptionType.Influencer));
+                    continue;
+                }
+
+                subscriptionTypes.Add(new Tuple<ApplicationUser, SubscriptionType>(user, SubscriptionType.Free));
+            }
+
+            return subscriptionTypes;
+        }
+
+        // Check whether user is Influencer by having MENTOR.GG in their name as we currently do not store it as a role in database
+        private bool IsInfluencer(string steamName)
+        {
+            return steamName.ToLowerInvariant().Contains("mentor.gg");
         }
 
     }
